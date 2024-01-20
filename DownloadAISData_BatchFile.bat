6116f8f2249e5fe78bf706009e097a477a487229
sbhurtya <bhurtyalsanjeev@gmail.com>
2024-01-20 10:46:54 -0600

    Updated batch file

--- DownloadAISData_BatchFile.bat
+++ DownloadAISData_BatchFile.bat
@@ -1,4 +1,5 @@
-cd C:\Users\sbhurtya\Box\WCS Forecast\Data\AIS Data\From Marine Cadastre
+:: Directory where the files will be downloaded
+cd AIS Data\From Marine Cadastre 
 
 ECHO ***********Downloading 2022 *******************
 wget -np -r -nH -L --cut-dirs=3 https://coast.noaa.gov/htdata/CMSP/AISDataHandler/2022/
